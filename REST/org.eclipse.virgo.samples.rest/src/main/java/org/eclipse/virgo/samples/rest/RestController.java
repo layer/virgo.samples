350b37d13991a28b264b61f73e73ff49616de635
Glyn Normington <gnormington@vmware.com>
2013-01-18 17:17:04 +0000

    next attempt

--- REST/org.eclipse.virgo.samples.rest/src/main/java/org/eclipse/virgo/samples/rest/RestController.java
+++ REST/org.eclipse.virgo.samples.rest/src/main/java/org/eclipse/virgo/samples/rest/RestController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -27,12 +28,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
  *
  */
 @Controller
-@RequestMapping("/users")
+//@RequestMapping("/rest/users")
 public final class RestController {
 
-    @RequestMapping(value="/{userId}", method = RequestMethod.GET)
+    @RequestMapping(value="/rest/users/{userId}", method = RequestMethod.GET)
+    @ResponseBody
     public String getUser(@PathVariable("userId") String userId) {
-        return null;
+        return userId;
     }
 
 }
